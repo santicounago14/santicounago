c54252e19b85d1852dde0d7b4810288a5aa81cf5
santicounago04 <santicounago.ort@gmail.com>
2025-02-19 07:29:40 -0300

    changes

--- carpeta03/src/components/SummaryScreen.jsx
+++ carpeta03/src/components/SummaryScreen.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 
 function SummaryScreen({ scannedCodes, playerName, onRestart }) {
+  // Mapeo de índice a tipo de carta
+  const cardTypeMapping = {
+    0: "Carta de contexto",
+    1: "Carta de problema",
+    2: "Carta de solución",
+    3: "Carta de insight",
+  };
+
   return (
     <div className="summary-screen">
-      <h2>Resumen del jugador: {playerName}</h2>
-      <h3>Códigos Escaneados:</h3>
-      <ul>
-        {scannedCodes.map((code, index) => (
-          <li key={index}>
-            Carta {index + 1}: {code}
-          </li>
-        ))}
-      </ul>
-      {/* Puedes agregar un botón para reiniciar el proceso, si lo deseas */}
+      <h2 className="resultsTitle">Resumen de {playerName}</h2>
+      <h3>Cartas escaneadas</h3>
+      {scannedCodes.map((code, index) => (
+        <div key={index} className="summary-card">
+          <h3 className={`card-type step-summary step-${index + 1} no-bg`}>
+            {cardTypeMapping[index] || "Carta"}
+          </h3>
+          <h4 className="card-content">{code}</h4>
+        </div>
+      ))}
+      <button className="verify-button">Verificar solución</button>
       {onRestart && <button onClick={onRestart}>Reiniciar</button>}
     </div>
   );
